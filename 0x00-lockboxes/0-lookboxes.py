956e72fe0cec410dcf702490a7c7b847f8823dbf
Wilder Rincon <1588@holbertonschool.com>
2021-04-22 20:43:44 -0500

    first commit

---
+++ 0x00-lockboxes/0-lookboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """Unlocks boxes"""
+
+    if type(boxes) is not list:
+        return False
+
+    if len(boxes) == 0:
+        return False
+
+    unlock = [0]
+    for key in unlock:
+        for element in boxes[key]:
+            if element not in unlock and element < len(boxes):
+                unlock.append(element)
+
+    if len(unlock) == len(boxes):
+        return True
+    return False
+  
