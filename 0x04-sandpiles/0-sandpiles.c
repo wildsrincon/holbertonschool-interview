73bb6080710f7d28d7cfe58862e1311a0a2dc1c2
Wilder Rincon <1588@holbertonschool.com>
2021-05-20 21:27:01 -0500

    Adding task 0

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "sandpiles.h"
+/**
+ * print_grid - print a grid
+ * @grid: sandpile
+ *
+ * Return: nothing
+ */
+void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+/**
+ * copy - copy a grid in another one
+ * @grid1: first sandpile
+ * @grid2: second sandpile
+ *
+ * Return: nothing
+ */
+void copy(int grid2[3][3], int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = grid2[i][j];
+		}
+	}
+}
+/**
+ * is_unstable - verify if the grid is unstable
+ * @grid1: sandpile
+ *
+ * Return: 1 if it is unstable or 0 if not
+ */
+int is_unstable(int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+				return (1);
+		}
+	}
+	return (0);
+}
+/**
+ * zeros - fill a grid with zeros
+ * @grid2: sandpile
+ *
+ * Return: nothing
+ */
+void zeros(int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid2[i][j] = 0;
+		}
+	}
+}
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: first sandpile
+ * @grid2: second sandpile
+ *
+ * Return: nothing
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+	}
+	while (is_unstable(grid1))
+	{
+		printf("=\n");
+		print_grid(grid1);
+		zeros(grid2);
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+				{
+					grid2[i][j] += grid1[i][j] - 4;
+					if (i - 1 >= 0)
+						grid2[i - 1][j] += 1;
+					if (j - 1 >= 0)
+						grid2[i][j - 1] += 1;
+					if (i + 1 <= 2)
+						grid2[i + 1][j] += 1;
+					if (j + 1 <= 2)
+						grid2[i][j + 1] += 1;
+				}
+				else
+					grid2[i][j] += grid1[i][j];
+			}
+		}
+		copy(grid2, grid1);
+	}
+}
