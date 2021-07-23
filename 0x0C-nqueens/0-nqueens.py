c65a1270b6115b5b694cb4fdf7e2657f4b3553ba
Wilder Rincon <1588@holbertonschool.com>
2021-07-22 20:04:58 -0500

    Adding solution task project 0x0C NQueens

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" Solving n queens """
+
+
+import sys
+# error handling for argv[1]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    N = sys.argv[1]
+    try:
+        N_int = int(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N_int < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+# n queens methods
+
+    coords = []
+
+    def isSafe(coords, row, col):
+        """ Checks if queen can be placed in coord of board.
+        Returns True if can, else False
+        """
+        rows = []
+        cols = []
+        diag_r = []
+        diag_l = []
+
+        for square in coords:
+            rows.append(square[0])
+            cols.append(square[1])
+            diag_r.append(square[0] + square[1])
+            diag_l.append(square[1] - square[0])
+
+        if row in rows or col in cols:
+            return False
+        if row + col in diag_r or col - row in diag_l:
+            return False
+
+        return True
+
+    def solveNqueens(coords, col, safe_queens=[]):
+        """ Creates array of queen positions
+        Returns array
+        """
+        for x in range(N_int):
+            if isSafe(coords, x, col):
+                coords.append([x, col])
+                if col == N_int - 1:
+                    safe_queens.append(coords.copy())
+                    del coords[-1]
+                else:
+                    solveNqueens(coords, col + 1)
+
+        if len(coords):
+            del coords[-1]
+        return safe_queens
+
+# sets base case for recursion
+
+    coords = solveNqueens(coords, 0)
+
+# prints coords of squares for safe queens
+
+    for squares in coords:
+        print(squares)
---
+++ 0x0C-nqueens/README.md
@@ -0,0 +1,15 @@
+# 0x0C. NQueens
+|![icon folder](https://img.icons8.com/fluent-systems-filled/20/216aff/opened-folder.png) Specializations > Web Stack programming > Front-end |
+|-------|
+|![icon user](https://img.icons8.com/fluent-systems-filled/20/216aff/user.png) By Wilder Rincon, Full-Stack Student at Holberton School|
+|[![icon linkedin](https://img.icons8.com/nolan/64/linkedin.png)](https://www.linkedin.com/in/wildsrincon/) [![icon github](https://img.icons8.com/nolan/64/github.png)](https://github.com/wildsrincon)|
+
+## Requirements
+### General
+- Allowed editors: vi, vim, emacs
+- All your files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+- All your files should end with a new line
+- The first line of all your files should be exactly #!/usr/bin/python3
+- A README.md file, at the root of the folder of the project, is mandatory
+- Your code should use the PEP 8 style (version 1.7.*)
+- All your files must be executable
