184be6170203302ba730ae92b823be29a6bd1951
Wilder Rincon <wildsrincon.developer@gmail.com>
2021-08-15 15:21:48 -0500

    changes permissions file 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,23 +1,64 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-const first = function () {
-  request(url, function (error, response, body) {
-    if (error) throw error;
-    second(JSON.parse(body).characters, 0);
-  });
-};
-
-const second = function (characters, i) {
-  if (characters.length === i) {
-    return;
-  }
-  request(characters[i], function (error, response, body) {
-    if (error) throw error;
-    console.log(JSON.parse(body).name);
-    second(characters, ++i);
-  });
-};
-
-first();
+/**
+ * 0x0F. Star Wars API
+ */
+ const process = require("process");
+ const request = require("request");
+ 
+ if (process.argv.length !== 3) {
+   process.exit(0);
+ }
+ 
+ const command1 = "films/" + process.argv[2];
+ const urlApi = "https://swapi-api.hbtn.io/api/";
+ const request1 = urlApi + command1;
+ let list = [];
+ let currentId = -1;
+ let limitMax = 0;
+ 
+ /**
+  * loadUrlApi fn that connect with api
+  *
+  * @return void and load default fn to read names
+  */
+ function loadUrlApi() {
+   request(request1, function (error, response, body) {
+     if (error) {
+       console.log("error:", error);
+       // throw error
+       process.exit(1);
+     }
+     list = JSON.parse(body).characters;
+     currentId = 0;
+     if (Array.isArray(list)) {
+       limitMax = list.length;
+       loadFilmName();
+     }
+   });
+ }
+ 
+ /**
+  * loadFilmName fn recursive that load film name
+  *
+  * @return void
+  */
+ function loadFilmName() {
+   if (limitMax > currentId) {
+     request(list[currentId], function (error, response, body) {
+       if (error) {
+         console.log("error:", error);
+         // throw error
+         process.exit(1);
+       }
+       const name = JSON.parse(body).name;
+       console.log(name);
+       loadFilmName();
+     });
+     currentId++;
+   }
+ }
+ 
+ /**
+  * loadUrlApi fn run script
+  */
+ loadUrlApi();
